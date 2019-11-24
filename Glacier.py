2696deb0af6ab091ff9fbfa7f580c9c002819b82
Ciaran O'Connor <c16354461@mydit.ie>
2019-11-24 19:40:14 +0000

    Add files via upload

--- Glacier.py
+++ Glacier.py
@@ -1,18 +1,33 @@
+# Step 1
+# Create two copies of the original image, and zero them out, this will be used later for masks,
+# but the images need to be the same size as the original images
+# Step2
+# Create a lower and upper range to specify the grey background in the new image,
+# create a mask from this range
+# Step3
+# Close the mask and then open it
+# Mask the old image using the mountain ROI from the new image
+# Step 4
+# Specify a range of white to create a mask that is areas of white that were present in the
+# old image where there is mountain in the new image
+# Step 5
+# Create contours from the white mask. Find the largest white contour by iterating through
+# every contour and comparing their area and marking the largest area contour location
+# Step 6
+# Create a mask of the largest white contour by drawing it in white on one of the blank
+# images created earlier, close the mask to remove false negative.
+# Merge the old glacier on to the new image
+# Step 7
+# Blend the edges of the glacier using the inpaint function. The mask is created from the outline
+# of the largest contour earlier
+# Step 8
+# Set variables for the drawing function and call the setMouseCallback, and imshow in a while loop
 import numpy as np
 import cv2
 import math
-from tkinter import *
 from matplotlib import pyplot as plt
 from matplotlib import image as image
 import easygui
-
-newImagef = None
-oldImagef = None
-
-def selectImage():
-    f = easygui.fileopenbox()
-
-    return f
 
 def closeMask(mask,x,y):
     shapeC = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(x,y))
@@ -48,70 +63,43 @@ def draw(event, x, y, flags, param):
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
 
-def im1Select():
-    oldImageSelect = selectImage()
+# Step 0
+f = "glacier.jpg"
+I = cv2.imread(f)
+I = cv2.cvtColor(I, cv2.COLOR_BGR2RGB)
+h,w,c = I.shape
+splitHeight = int(h/2)
+oldImage = I[0:splitHeight,:,:]
+newImage = I[splitHeight:(2*splitHeight),:,:]
 
-    global oldImagef
-    oldImagef = oldImageSelect
+# f = easygui.fileopenbox()
+# newImage = cv2.imread(f)
+#
+# f2 = easygui.fileopenbox()
+# oldImage = cv2.imread(f2)
 
-
-
-def im2Select():
-    newImageSelect = selectImage()
-
-    global newImagef
-    newImagef = newImageSelect
-
-
-window = Tk() 
-window.geometry("500x500")
-window.title("Repeat Photography")
-
-label1=Label(window, text="Quantify Glacial Erosion \n in two photos", relief="solid", width=20, font=("arial",19,"bold"))
-label1.place(x=90,y=53)
-
-im1Button = Button(window, text= "Old Image", width= 12, command=im1Select, bg="brown", fg="white")
-im1Button.place(x=130,y=360)
-
-im2Button = Button(window, text= "New Image", width= 12, command=im2Select, bg="brown", fg="white")
-im2Button.place(x=260,y=360)
-
-GoButton = Button(window, text="GO",width = 12, command=window.destroy)
-GoButton.place(x=200, y=280)
-
- 
-window.mainloop() 
-
-oldImage = cv2.imread(oldImagef)
-newImage = cv2.imread(newImagef)
-
-# Create two copies of the original image, and zero them out, this will be used later for masks,
-# but the images need to be the same size as the original images
+# Step 1
 glacierMask = oldImage.copy()
 glacierMaskBorder = oldImage.copy()
 glacierMask[:,:,:] = 0
 glacierMaskBorder[:,:,:] = 0
 
-# Create a lower and upper range to specify the grey background in the new image,
-# create a mask from this range
+# Step 2
 lower = np.array([0,0,0])
 upper = np.array([140,140,140])
 greyMask = cv2.inRange(newImage,lower,upper)
 
-# Close the mask and then open it
+# Step 3
 greyMask = closeMask(greyMask,20,30)
 greyMask = openMask(greyMask,15,15)
-
-# Mask the old image using the mountain ROI from the new image
 mountain = cv2.bitwise_and(oldImage,oldImage,mask=greyMask)
 
-# Specify a range of white to create a mask that is areas of white that were present in the
-# old image where there is mountain in the new image
+# Step 4
 lower = np.array([170,150,150])
 upper = np.array([255,255,255])
 whiteMask = cv2.inRange(mountain,lower,upper)
 
-# Create contours from the white mask, then find the largest white contour and mark its location
+# Step 5
 contours, hierarchy = cv2.findContours(whiteMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 area = 0
 i = 0
@@ -121,14 +109,12 @@ for contour in contours:
         contourI = i
     i += 1
 
-# Create a mask of the largest white contour by drawing it in white on one of the blank
-# images created earlier, close the mask to remove false negative.
-# Merge the old glacier on to the new image
+# Step 6
 cv2.drawContours(glacierMask,contours,contourI,(255,255,255),-1)
 glacierMask = closeMask(glacierMask,15,15)
 oldGlacierNew = JoinOnMask(oldImage,newImage,glacierMask)
 
-# Blend the edges of the glacier using the inpaint function, and the glacier contour as the mask
+# Step 7
 glacierMask = cv2.cvtColor(glacierMask, cv2.COLOR_BGR2GRAY)
 glacierContour, hierarchy = cv2.findContours(glacierMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(glacierMaskBorder,glacierContour,0,(255,255,255),2)
@@ -136,7 +122,7 @@ glacierMaskBorder = cv2.cvtColor(glacierMaskBorder, cv2.COLOR_BGR2GRAY)
 glacierMerged = cv2.inpaint(oldGlacierNew,glacierMaskBorder, 1, cv2.INPAINT_TELEA)
 glacierMerged = cv2.cvtColor(glacierMerged, cv2.COLOR_BGR2RGB)
 
-# Set variables for the drawing function and call the setMouseCallback, and imshow in a while loop
+# Step 8
 windowName = 'Drawing'
 drawing = False
 userMask = newImage.copy()
@@ -151,13 +137,3 @@ while (True):
         break
 
 cv2.destroyAllWindows()
-
-# plt.subplot(2, 2, 1)
-# plt.imshow(oldImage)
-# plt.subplot(2, 2, 3)
-# plt.imshow(newImage)
-# plt.subplot(2, 2, 2)
-# plt.imshow(whiteMask)
-# plt.subplot(2, 2, 4)
-# plt.imshow(glacierMerged)
-# plt.show()
